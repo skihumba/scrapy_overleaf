8de57f8c184280a6ec28cce5262ca50b4776f83d
skihumba <abmiuhiks@gmail.com>
2021-04-09 00:05:22 +0300

    data extraction & dumping to json file successfull

--- overleaf/overleaf/spiders/cv_temp.py
+++ overleaf/overleaf/spiders/cv_temp.py
@@ -7,31 +7,12 @@ class CvTempSpider(scrapy.Spider):
     start_urls = ['https://www.overleaf.com/latex/templates/tagged/cv']
 
     def parse(self, response):
-        # links =  response.xpath('//div[@class="gallery-thumbnail"]/a/@href').extract()
-        # for link in links:
-        #     self.log(response.urljoin(link))
-        # images = response.xpath('//img[@class="thumbnail"]/@src').extract()
-        # for img in images:
-        #     self.log(img)
         for element in response.xpath('.//div[@class="gallery-thumbnail"]'):
-            # yield{
-            # #    'cv_url' : response.urljoin(i for i in response.xpath('//div[@class="gallery-thumbnail"]/a/@href').extract()), 
-            # #    'cv_url' : response.urljoin(element.xpath('.//div[@class="gallery-thumbnail"]/a/@href').extract()),
-            # #    'cv_url' : element.xpath('.//div[@class="gallery-thumbnail"]/a/@href').extract(),
-            #    'cv_image_url' : element.xpath('.//img[@class="thumbnail"]/@src').extract()
-            # }
-
-            # cv_url = response.urljoin(response.xpath('//div[@class="gallery-thumbnail"]/a/@href').extract())
-            # self.log(cv_url)
-
-            # cv_url = response.urljoin(str(element.xpath('.//a/@href').extract())) <--PROMISING!!
-            tmp_cv_url = element.xpath('.//a/@href').extract()
-            cv_url = response.urljoin(tmp_cv_url)
-            # cv_url = response.urljoin(temp_cv_url)
-
-            cv_image_url = element.xpath('.//img[@class="thumbnail"]/@src').extract()
-
-            yield {
-                'cv_url' : cv_url,
-                'cv_image_url' : cv_image_url
+            yield{
+               'cv_url' : response.urljoin(element.xpath('.//a/@href').get()), 
+               'cv_image_url' : element.xpath('.//img[@class="thumbnail"]/@src').get()
             }
+
+        next_page_url = response.xpath('//a[@aria-label="Go to next page"]/@href').get()
+        if next_page_url is not None:
+            yield scrapy.Request(response.urljoin(next_page_url))
