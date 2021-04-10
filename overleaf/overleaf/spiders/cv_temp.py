5c9b688cc7e5aa4a09d1356062bc1e840c9a1ae7
skihumba <abmiuhiks@gmail.com>
2021-04-10 11:26:38 +0300

    clean up

--- overleaf/overleaf/spiders/cv_temp.py
+++ overleaf/overleaf/spiders/cv_temp.py
@@ -8,11 +8,27 @@ class CvTempSpider(scrapy.Spider):
 
     def parse(self, response):
         for element in response.xpath('.//div[@class="gallery-thumbnail"]'):
+            cv_url = response.urljoin(element.xpath('.//a/@href').get())
+            cv_image_url = element.xpath('.//img[@class="thumbnail"]/@src').get()
+
             yield{
-               'cv_url' : response.urljoin(element.xpath('.//a/@href').get()), 
-               'cv_image_url' : element.xpath('.//img[@class="thumbnail"]/@src').get()
+               'cv_url' : cv_url, 
+               'cv_image_url' : cv_image_url
             }
 
-        next_page_url = response.xpath('//a[@aria-label="Go to next page"]/@href').get()
-        if next_page_url is not None:
-            yield scrapy.Request(response.urljoin(next_page_url))
+            # next page(s)
+            next_page_url = response.xpath('//a[@aria-label="Go to next page"]/@href').get()
+            if next_page_url is not None:
+                yield response.follow(response.urljoin(next_page_url), callback=self.parse)
+
+            # create web page with cv images as hyperlinks to the cv url
+            html = ""
+            html += """<a href="{cv_url}"
+            target="_blank">
+            <img src="{cv_image_url}" height="50%" width="30%"/>
+            <a/>.
+            """.format(cv_url=cv_url, cv_image_url=cv_image_url)
+
+            with open("./data/cv_templates.html", mode="a") as web_page:
+                web_page.write(html)
+                web_page.close()
